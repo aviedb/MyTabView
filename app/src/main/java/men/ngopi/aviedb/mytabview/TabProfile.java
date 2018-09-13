27465b30e0d115c3bc98ccf0eb1192c19c9e9a78
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-09-13 08:43:57 +0700

    Add Spinner

--- app/src/main/java/men/ngopi/aviedb/mytabview/TabProfile.java
+++ app/src/main/java/men/ngopi/aviedb/mytabview/TabProfile.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class TabProfile extends Fragment {
@@ -13,6 +17,25 @@ public class TabProfile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_profile, container, false);
+
+        // Jabatan Spinner
+        Spinner jabatanSpinner = rootView.findViewById(R.id.jabatanSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(rootView.getContext(),
+                R.array.jabatanSpinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        jabatanSpinner.setAdapter(adapter);
+
+        // Gender Spinner
+        Spinner genderSpinner = rootView.findViewById(R.id.genderSpinner);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(rootView.getContext(),
+                R.array.genderArray, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(adapter1);
+
+        EditText jabatanET = rootView.findViewById(R.id.jabatanTF);
+        jabatanET.setText("      ");
+        EditText genderET = rootView.findViewById(R.id.genderTF);
+        genderET.setText("       ");
 
         return rootView;
     }
