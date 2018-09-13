168748cc0ce89f3e76ae80d93d9f38103bdac2f4
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-09-13 08:49:02 +0700

    Move FloatingActionButton from activity_main

--- app/src/main/java/men/ngopi/aviedb/mytabview/TabSetting.java
+++ app/src/main/java/men/ngopi/aviedb/mytabview/TabSetting.java
@@ -1,6 +1,8 @@
 package men.ngopi.aviedb.mytabview;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,15 @@ public class TabSetting extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_setting, container, false);
+
+        FloatingActionButton fab = rootView.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         return rootView;
     }
